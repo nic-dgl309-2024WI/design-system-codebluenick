12d0d0c7b85377ecea18ecfcc916e4ffa1617621
codebluenick <codebluenick@gmail.com>
2024-02-06 16:26:45 -0800

    Layout fix

--- design-system.js
+++ design-system.js
@@ -1,1 +1,19 @@
-//Add js later
+//Add more js later
+function toggleNavbar(isVisible) {
+    let sideNavbar = document.getElementById("side-navbar");
+    let navbarToggle = document.getElementById("navbar-toggle");
+    if (isVisible) {
+        // When isVisible is true (showing the navbar)
+        document.getElementById("navbar-open").style.display = "none";
+        document.getElementById("navbar-close").style.display = "flex";
+        sideNavbar.style.display = "flex";
+        navbarToggle.classList.add("active");
+    } else {
+        // When isVisible is false (hiding the navbar)
+        document.getElementById("navbar-open").style.display = "flex";
+        document.getElementById("navbar-close").style.display = "none";
+        sideNavbar.style.display = "none";
+        navbarToggle.classList.remove("active");
+        logo.style.display = "none";
+    }
+}
