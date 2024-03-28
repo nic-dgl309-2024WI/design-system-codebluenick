62647657f5fde3ae9155ddb7b7d6a710ba070284
codebluenick <codebluenick@gmail.com>
2024-03-27 22:09:33 -0700

    Add styles and responsiveness for mobile navbar

--- Design_System/design-system.js
+++ Design_System/design-system.js
@@ -1,18 +1,18 @@
 //Add more js later
 function toggleNavbar(isVisible) {
-    let sideNavbar = document.getElementById("ds-side-navbar");
+    let dsSideNavbar = document.getElementById("ds-side-navbar");
     let navbarToggle = document.getElementById("navbar-toggle");
     if (isVisible) {
         // When isVisible is true (showing the navbar)
         document.getElementById("navbar-open").style.display = "none";
         document.getElementById("navbar-close").style.display = "flex";
-        sideNavbar.style.display = "flex";
+        dsSideNavbar.style.display = "flex";
         navbarToggle.classList.add("active");
     } else {
         // When isVisible is false (hiding the navbar)
         document.getElementById("navbar-open").style.display = "flex";
         document.getElementById("navbar-close").style.display = "none";
-        sideNavbar.style.display = "none";
+        dsSideNavbar.style.display = "none";
         navbarToggle.classList.remove("active");
         logo.style.display = "none";
     }
@@ -32,4 +32,3 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //js to toggle side-nav-bar in mobile screen (Outback Nursery)
-
