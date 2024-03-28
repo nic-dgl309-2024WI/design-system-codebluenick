2fc7735bff184a00fffe4a9129baa997231ff596
codebluenick <codebluenick@gmail.com>
2024-03-27 23:28:58 -0700

    Add js to toggle menu click (ON)

--- Design_System/design-system.js
+++ Design_System/design-system.js
@@ -32,3 +32,28 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //js to toggle side-nav-bar in mobile screen (Outback Nursery)
+document.addEventListener("DOMContentLoaded", function() {
+  let on_SideNavbar = document.getElementById("on-side-navbar");
+  let on_navbarOpen = document.getElementById("on-navbar-open");
+  let on_navbarClose = document.getElementById("on-navbar-close");
+
+  // Optional: Assuming you have a logo that needs to be hidden
+  //let logo = document.querySelector(".on-logo"); // Adjust the selector as necessary
+
+  // Function to show the side navbar
+  on_navbarOpen.addEventListener("click", function() {
+  on_SideNavbar.style.transform = "translateX(0%)"; // Assuming CSS transitions
+  this.style.display = "none";
+  on_navbarClose.style.display = "flex";
+  //if (logo) logo.style.display = "none"; // Hide logo if it exists
+  });
+
+  // Function to hide the side navbar
+  on_navbarClose.addEventListener("click", function() {
+  on_SideNavbar.style.transform = "translateX(-100%)"; // Assuming CSS transitions
+  this.style.display = "none";
+  on_navbarOpen.style.display = "flex";
+  //if (logo) logo.style.display = "flex"; // Show logo if it exists
+  });
+});
+
